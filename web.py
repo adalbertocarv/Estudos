040b2607520352d77c33c5c52f4232ce4bcf557d
Adalberto Carvalho <107323618+adalbertocarv@users.noreply.github.com>
2023-03-30 16:06:07 -0300

    Update web.py

--- web.py
+++ web.py
@@ -2,25 +2,25 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
 
-# Definindo o caminho para o driver do Chrome
+# Caminho do drive
 driver_path = 'C:\\Users\\adalb\\Downloads\\chromedriver_win32\\chromedriver.exe'
 
-# determinando o driver do Chrome
+# drive do chrome
 driver = webdriver.Chrome(executable_path=driver_path)
 
-# Acessar o site do Medium
+# Acessar o site
 driver.get('https://medium.com/')
 
 # Esperar a pagina carregar 
 time.sleep(2)
 
-# Rolar a pagina
+# Descer a pagina
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-# esperando mais um tempo
+# esperando a pagina
 time.sleep(2)
 
-# Encontrando o primeiro post
+# Encontrando post aleatorio
 post_aleatorio = driver.find_element(By.XPATH,'//*[@id="root"]/div/div[4]/div[3]/div[1]/div/div/section/div/div/div[1]/div[9]/div/div/div/a')
 post_aleatorio.click()
 
